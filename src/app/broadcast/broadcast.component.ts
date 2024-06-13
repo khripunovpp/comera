0e6debddcc4e9c8955254c8f0cd8ac98857f9e0b
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2024-06-13 21:24:18 +0100

    parts detection

--- src/app/broadcast/broadcast.component.ts
+++ src/app/broadcast/broadcast.component.ts
@@ -14,10 +14,15 @@ declare var tf: any;
 export class BroadcastComponent {
   status = signal('status');
   video = viewChild<ElementRef<HTMLVideoElement>>('webcam');
-  liveView = viewChild<ElementRef<HTMLElement>>('liveView');
+  dots = viewChild<ElementRef<HTMLElement>>('dots');
 
   model: any = null;
-  children: any[] = [];
+
+  picsCount = 5;
+  cropPoint = [170, 15];
+  cropWidth = 345;
+  MODEL_PATH = 'https://tfhub.dev/google/tfjs-model/movenet/singlepose/lightning/4';
+  pointWidth = 4;
 
   onButtonClick(e: Event) {
     if (this.getUserMediaSupported()) {
@@ -32,11 +37,169 @@ export class BroadcastComponent {
       if (this.status()) {
         this.status.set('Loaded TensorFlow.js - version: ' + tf.version.tfjs);
       }
-      cocoSsd.load().then((loadedModel: any) => {
-        console.log('Model loaded.')
-        this.model = loadedModel;
-      });
+      this.loadModel();
+
     }
+  }
+
+  async loadModel() {
+    this.model = await tf.loadGraphModel(this.MODEL_PATH, {fromTFHub: true});
+    if (this.status()) {
+      this.status.set('Model loaded');
+    }
+  }
+
+  putDot(xn: any, yn: any, color: any) {
+    const dot = document.createElement('div');
+    dot.style.position = 'absolute';
+    dot.style.width = `${this.pointWidth}px`;
+    dot.style.height = `${this.pointWidth}px`;
+    dot.style.borderRadius = '50%';
+    dot.style.backgroundColor = color;
+    const [x, y] = [
+      (xn * 345) + this.cropPoint[0] - (this.pointWidth / 2),
+      (yn * 345) + this.cropPoint[1] - (this.pointWidth / 2)
+    ];
+    dot.style.top = `${y}px`;
+    dot.style.left = `${x}px`;
+    this.dots()!.nativeElement.appendChild(dot);
+    return dot;
+  }
+
+  async drawPoints(cords: any) {
+    this.putDot(cords.nose.x, cords.nose.y, 'red');
+    this.putDot(cords.leftEye.x, cords.leftEye.y, 'blue');
+    this.putDot(cords.rightEye.x, cords.rightEye.y, 'blue');
+    this.putDot(cords.leftEar.x, cords.leftEar.y, 'blue');
+    this.putDot(cords.rightEar.x, cords.rightEar.y, 'blue');
+    this.putDot(cords.leftShoulder.x, cords.leftShoulder.y, 'blue');
+    this.putDot(cords.rightShoulder.x, cords.rightShoulder.y, 'blue');
+    this.putDot(cords.leftElbow.x, cords.leftElbow.y, 'blue');
+    this.putDot(cords.rightElbow.x, cords.rightElbow.y, 'blue');
+    this.putDot(cords.leftWrist.x, cords.leftWrist.y, 'blue');
+    this.putDot(cords.rightWrist.x, cords.rightWrist.y, 'blue');
+    this.putDot(cords.leftHip.x, cords.leftHip.y, 'blue');
+    this.putDot(cords.rightHip.x, cords.rightHip.y, 'blue');
+    this.putDot(cords.leftKnee.x, cords.leftKnee.y, 'blue');
+    this.putDot(cords.rightKnee.x, cords.rightKnee.y, 'blue');
+    this.putDot(cords.leftAnkle.x, cords.leftAnkle.y, 'blue');
+    this.putDot(cords.rightAnkle.x, cords.rightAnkle.y, 'blue');
+  }
+
+  parseCords(arrayOutput: any) {
+    const points = arrayOutput[0][0];
+    return {
+      nose: {
+        x: points[0][1],
+        y: points[0][0],
+        confidence: points[0][2]
+      },
+      leftEye: {
+        x: points[1][1],
+        y: points[1][0],
+        confidence: points[1][2]
+      },
+      rightEye: {
+        x: points[2][1],
+        y: points[2][0],
+        confidence: points[2][2]
+      },
+      leftEar: {
+        x: points[3][1],
+        y: points[3][0],
+        confidence: points[3][2]
+      },
+      rightEar: {
+        x: points[4][1],
+        y: points[4][0],
+        confidence: points[4][2]
+      },
+      leftShoulder: {
+        x: points[5][1],
+        y: points[5][0],
+        confidence: points[5][2]
+      },
+
+      rightShoulder: {
+        x: points[6][1],
+        y: points[6][0],
+        confidence: points[6][2]
+      },
+      leftElbow: {
+        x: points[7][1],
+        y: points[7][0],
+        confidence: points[7][2]
+      },
+      rightElbow: {
+        x: points[8][1],
+        y: points[8][0],
+        confidence: points[8][2]
+      },
+      leftWrist: {
+        x: points[9][1],
+        y: points[9][0],
+        confidence: points[9][2]
+      },
+      rightWrist: {
+        x: points[10][1],
+        y: points[10][0],
+        confidence: points[10][2]
+      },
+      leftHip: {
+        x: points[11][1],
+        y: points[11][0],
+        confidence: points[11][2]
+      },
+      rightHip: {
+        x: points[12][1],
+        y: points[12][0],
+        confidence: points[12][2]
+      },
+      leftKnee: {
+        x: points[13][1],
+        y: points[13][0],
+        confidence: points[13][2]
+      },
+      rightKnee: {
+        x: points[14][1],
+        y: points[14][0],
+        confidence: points[14][2]
+      },
+      leftAnkle: {
+        x: points[15][1],
+        y: points[15][0],
+        confidence: points[15][2]
+      },
+      rightAnkle: {
+        x: points[16][1],
+        y: points[16][0],
+        confidence: points[16][2]
+      }
+    }
+  }
+
+  cropImage(image: any, x: any, y: any, width: any) {
+    const cropStartPoint = [y, x];
+    const cropSize = [width, width];
+    return tf.slice(image, cropStartPoint, cropSize);
+  }
+
+  async calculate(
+    img: any,
+  ) {
+    // this.dots()!.nativeElement.innerHTML = '';
+    let imageTensor = tf.browser.fromPixels(img);
+    let croppedImage = this.cropImage(imageTensor, this.cropPoint[0], this.cropPoint[1], this.cropWidth);
+    let resizedImage = tf.image.resizeBilinear(croppedImage, [192, 192], true).toInt();
+    let tensorOutput = this.model.predict(tf.expandDims(resizedImage));
+    let arrayOutput = await tensorOutput.array();
+    let cords = this.parseCords(arrayOutput)
+   await this.drawPoints(cords);
+    imageTensor.dispose();
+    croppedImage.dispose();
+    resizedImage.dispose();
+    tensorOutput.dispose();
+
   }
 
   getUserMediaSupported() {
@@ -69,51 +232,15 @@ export class BroadcastComponent {
       });
   }
 
-  windows: any[] = [];
-
   predictWebcam() {
     if (!this.model) {
       return;
     }
 
+
     // Now let's start classifying a frame in the stream.
-    this.model.detect(this.video()?.nativeElement).then((predictions: any) => {
-      // Remove any highlighting we did previous frame.
-      for (let i = 0; i < this.children.length; i++) {
-        this.liveView()?.nativeElement.removeChild(this.children[i]);
-      }
-      this.children.splice(0);
+    this.calculate(this.video()?.nativeElement).then((predictions: any) => {
 
-      // Now lets loop through predictions and draw them to the live view if
-      // they have a high confidence score.
-      for (let n = 0; n < predictions.length; n++) {
-        // If we are over 66% sure we are sure we classified it right, draw it!
-        if (predictions[n].score > 0.66) {
-          const p = document.createElement('p');
-          p.innerText = predictions[n].class + ' - with '
-            + Math.round(parseFloat(predictions[n].score) * 100)
-            + '% confidence.';
-          // @ts-ignore
-          p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-            + (predictions[n].bbox[1] - 10) + 'px; width: '
-            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
-
-          const highlighter = document.createElement('div');
-          highlighter.setAttribute('class', 'highlighter');
-          // @ts-ignore
-          highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: '
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
-
-          this.liveView()?.nativeElement.appendChild(highlighter);
-          this.liveView()?.nativeElement.appendChild(p);
-          this.children.push(highlighter);
-          this.children.push(p);
-        }
-      }
-
-      // Call this function again to keep predicting when the browser is ready.
       window.requestAnimationFrame(this.predictWebcam.bind(this));
     }).catch((error: any) => {
       console.log(error);
